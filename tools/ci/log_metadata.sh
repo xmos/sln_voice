2c659f786dfa5f1eb87b295afed4cd442956ce6f
keithm-xmos <35048036+keithm-xmos@users.noreply.github.com>
2022-05-26 17:12:28 -0400

    added more metadata

--- tools/ci/log_metadata.sh
+++ tools/ci/log_metadata.sh
@@ -6,7 +6,7 @@
 set -e
 
 metadata_filename="$1"
-template='{"actor":"%s","event":"%s","head_ref":"%s","ref":"%s","run_id":"%s","sha":"%s"}'
-json_string=$(printf "$template" "$GITHUB_ACTOR" "$GITHUB_EVENT_NAME" "$GITHUB_HEAD_REF" "$GITHUB_REF" "$GITHUB_RUN_ID" "$GITHUB_SHA")
+template='{"actor":"%s","event":"%s","base_ref":"%s","head_ref":"%s","ref":"%s","run_id":"%s","sha":"%s"}'
+json_string=$(printf "$template" "$GITHUB_ACTOR" "$GITHUB_EVENT_NAME" "$GITHUB_BASE_REF" "$GITHUB_HEAD_REF" "$GITHUB_REF" "$GITHUB_RUN_ID" "$GITHUB_SHA")
 echo "Build metadata: $json_string"
 echo "$json_string" >> $metadata_filename
