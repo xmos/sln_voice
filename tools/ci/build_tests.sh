0818a777af5aafc67f97f0629f3ffb7b80e1584e
keithm-xmos <35048036+keithm-xmos@users.noreply.github.com>
2022-05-26 14:42:39 -0400

    added build scripts for ci

--- tools/ci/build_tests.sh
+++ tools/ci/build_tests.sh
@@ -12,11 +12,8 @@ mkdir -p ${DIST_DIR}
 # setup configurations
 # row format is: "name make_target BOARD toolchain"
 applications=(
-    "stlp_int_adec                application_stlp_int_adec                XK_VOICE_L71       xmos_cmake_toolchain/xs3a.cmake"
-    "stlp_int_adec_altarch        application_stlp_int_adec_altarch        XK_VOICE_L71       xmos_cmake_toolchain/xs3a.cmake"
     "stlp_ua_adec                 application_stlp_ua_adec                 XK_VOICE_L71       xmos_cmake_toolchain/xs3a.cmake"
     "stlp_ua_adec_altarch         application_stlp_ua_adec_altarch         XK_VOICE_L71       xmos_cmake_toolchain/xs3a.cmake"
-    "ffd                          application_ffd                          XK_VOICE_L71       xmos_cmake_toolchain/xs3a.cmake"
 )
 
 # perform builds
@@ -33,6 +30,6 @@ for ((i = 0; i < ${#applications[@]}; i += 1)); do
 
     (cd ${path}; rm -rf build_${board})
     (cd ${path}; mkdir -p build_${board})
-    (cd ${path}/build_${board}; log_errors cmake ../ -DCMAKE_TOOLCHAIN_FILE=${toolchain_file} -DBOARD=${board}; log_errors make ${make_target} -j)
+    (cd ${path}/build_${board}; log_errors cmake ../ -DCMAKE_TOOLCHAIN_FILE=${toolchain_file} -DBOARD=${board} -DDEBUG_STLP_USB_MIC_INPUT=1; log_errors make ${make_target} -j)
     (cd ${path}/build_${board}; cp ${make_target}.xe ${DIST_DIR})
 done
