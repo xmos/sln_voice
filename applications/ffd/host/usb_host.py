e62e3cb6ed8d46cbbf46431109b07ccbed9a0b4a
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2022-06-06 17:06:49 -0400

    Remove Python 3.10 requirement

--- applications/ffd/host/usb_host.py
+++ applications/ffd/host/usb_host.py
@@ -49,45 +49,5 @@ while True:
 
             if int_val in intent_to_text.keys():
                 print(intent_to_text[int_val])
-
-            # match int_val:
-            #     case 100:
-            #         print("Hello XMOS");
-            #     case 110:
-            #         print("Hello Wanson");
-            #     case 200:
-            #         print("Switch On the TV");
-            #     case 210:
-            #         print("Switch Off the TV");
-            #     case 220:
-            #         print("Channel Up");
-            #     case 230:
-            #         print("Channel Down");
-            #     case 240:
-            #         print("Volume Up");
-            #     case 250:
-            #         print("Volume Down");
-            #     case 300:
-            #         print("Switch On the Lights");
-            #     case 310:
-            #         print("Switch Off the Lights");
-            #     case 320:
-            #         print("Brightness Up");
-            #     case 330:
-            #         print("Brightness Down");
-            #     case 400:
-            #         print("Switch On the Fan");
-            #     case 410:
-            #         print("Switch Off the Fan");
-            #     case 420:
-            #         print("Speed Up the Fan");
-            #     case 430:
-            #         print("Slow Down the Fan");
-            #     case 440:
-            #         print("Set Higher Temperature");
-            #     case 450:
-            #         print("Set Lower Temperature");
-            #     case _:
-            #         continue
     except:
         continue
