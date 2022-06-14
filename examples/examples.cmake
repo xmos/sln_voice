48533ff2cd818bd2f3b3d8aa56ffcc9eda653c1c
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2022-06-14 13:28:18 -0400

    Add audio mux example to enable stlp testing

--- examples/examples.cmake
+++ examples/examples.cmake
@@ -1,4 +1,5 @@
 
 if(${CMAKE_SYSTEM_NAME} STREQUAL XCORE_XS3A)
+    include(${CMAKE_CURRENT_LIST_DIR}/audio_mux/audio_mux.cmake)
     include(${CMAKE_CURRENT_LIST_DIR}/keyword_spotter/keyword_spotter.cmake)
 endif()
