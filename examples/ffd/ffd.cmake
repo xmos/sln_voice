9bf5eee780ee3f3bbbb4b9010640747fb4cf123a
Jon Carrier <jonathancarrier@xmos.com>
2022-11-15 09:55:13 -0500

    Updated ffd.cmake APP_INCLUDES
    Added other subfolders in ffd/src to APP_INCLUDES.

--- examples/ffd/ffd.cmake
+++ examples/ffd/ffd.cmake
@@ -4,8 +4,11 @@
 file(GLOB_RECURSE APP_SOURCES ${CMAKE_CURRENT_LIST_DIR}/src/*.c )
 set(APP_INCLUDES
     ${CMAKE_CURRENT_LIST_DIR}/src
-    ${CMAKE_CURRENT_LIST_DIR}/src/ssd1306
+    ${CMAKE_CURRENT_LIST_DIR}/src/audio_pipeline
+    ${CMAKE_CURRENT_LIST_DIR}/src/gpio_ctrl
+    ${CMAKE_CURRENT_LIST_DIR}/src/intent_handler
     ${CMAKE_CURRENT_LIST_DIR}/src/intent_handler/audio_response
+    ${CMAKE_CURRENT_LIST_DIR}/src/ssd1306
 )
 set(RTOS_CONF_INCLUDES
     ${CMAKE_CURRENT_LIST_DIR}/src/rtos_conf
