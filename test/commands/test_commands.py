bd05ce55a511030ac6501c3bad95a31e3133904e
jrshaferXMOS <johnshafer@xmos.com>
2023-01-27 14:04:39 -0500

    add commands pytest

---
+++ test/commands/test_commands.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2022 XMOS LIMITED. This Software is subject to the terms of the
+# XMOS Public License: Version 1
+
+def test_results(log):
+    errors = []
+    with open(log, 'r+') as f:
+        lines = f.readlines()
+        for line in lines:
+            fields = line.split(',')
+            fields = [s.strip() for s in fields]
+            # expected format:
+            # ['filename="wav_name.wav"', 'detected=25', 'min=20', 'max=25']
+            values = []
+            for field in fields:
+                values.append(field.split('=', 1)[-1])
+            # strip quotes
+            filename = values[0].replace('"', '')
+            detections = int(values[1])
+            min_detect = int(values[2])
+            max_detect = int(values[3])
+            if not min_detect <= detections:
+                errors.append(filename + " failed with " + str(detections) + " detections.")
+            if not detections <= max_detect:
+                errors.append(filename + " failed with " + str(detections) + " detections.")
+
+    assert not errors, "Test failed:\n{}".format("\n".join(errors))
