08c068a40e8666253f7d48282a4fbaec0df5e103
Jon Carrier <jonathancarrier@xmos.com>
2023-02-03 18:14:23 -0500

    Refactored low power audio buf args/vars

--- examples/ffd/src/power/low_power_audio_buffer.c
+++ examples/ffd/src/power/low_power_audio_buffer.c
@@ -1,5 +1,5 @@
-// Copyright (c) 2022 XMOS LIMITED. This Software is subject to the terms of the
-// XMOS Public License: Version 1
+// Copyright (c) 2022-2023 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public License: Version 1
 
 /* System headers */
 #include <stdint.h>
@@ -43,12 +43,12 @@ ring_buffer_t ring_buf = {
 
 #endif // LOW_POWER_AUDIO_BUFFER_ENABLED
 
-void low_power_audio_buffer_enqueue(int32_t *frames, size_t num_frames)
+void low_power_audio_buffer_enqueue(int32_t *samples, size_t num_samples)
 {
 #if LOW_POWER_AUDIO_BUFFER_ENABLED
     const uint32_t tail_addr = ((uint32_t)ring_buf.buf + (ring_buf.size * sizeof(int32_t)));
 
-    size_t total_bytes = num_frames * sizeof(int32_t);
+    size_t total_bytes = num_samples * sizeof(int32_t);
     size_t tail_bytes = ring_buf.size - ((uint32_t)ring_buf.set_ptr - (uint32_t)ring_buf.buf);
 
     if (tail_bytes > total_bytes)
@@ -56,8 +56,8 @@ void low_power_audio_buffer_enqueue(int32_t *frames, size_t num_frames)
 
     uint32_t head_bytes = total_bytes - tail_bytes;
 
-    memcpy(ring_buf.set_ptr, (char *)frames, tail_bytes);
-    memcpy(ring_buf.buf, (char *)frames + tail_bytes, head_bytes);
+    memcpy(ring_buf.set_ptr, (char *)samples, tail_bytes);
+    memcpy(ring_buf.buf, (char *)samples + tail_bytes, head_bytes);
 
     if (head_bytes)
         ring_buf.set_ptr = (char *)ring_buf.buf + head_bytes;
@@ -67,13 +67,13 @@ void low_power_audio_buffer_enqueue(int32_t *frames, size_t num_frames)
     if ((uint32_t)ring_buf.set_ptr >= tail_addr)
         ring_buf.set_ptr = (char *)ring_buf.buf;
 
-    ring_buf.full = ((ring_buf.count + num_frames) >= ring_buf.size);
+    ring_buf.full = ((ring_buf.count + num_samples) >= ring_buf.size);
 
     if (ring_buf.full) {
         ring_buf.get_ptr = ring_buf.set_ptr;
         ring_buf.count = ring_buf.size;
     } else {
-        ring_buf.count += num_frames;
+        ring_buf.count += num_samples;
     }
 
     ring_buf.empty = (ring_buf.count == 0);
