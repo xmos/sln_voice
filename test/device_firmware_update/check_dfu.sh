3795f305456fb08d20240ff112568036b96538b3
hp <johnshafer@xmos.com>
2023-03-02 09:27:22 -0500

    take out udev rule, change dfu-util device id from 0020 to 4001

--- test/device_firmware_update/check_dfu.sh
+++ test/device_firmware_update/check_dfu.sh
@@ -64,10 +64,10 @@ export_tools_version
 xflash ${ADAPTER_ID} --factory-version ${XTC_VERSION_MAJOR}.${XTC_VERSION_MINOR} --upgrade 0 ${FIRMWARE} -o ${OUTPUT_DIR}/${FIRMWARE_NAME}_upgrade.bin
 
 # write the upgrade image
-dfu-util -e -d 0020 -a 1 -D ${OUTPUT_DIR}/${FIRMWARE_NAME}_upgrade.bin --reset
+dfu-util -e -d 4001 -a 1 -D ${OUTPUT_DIR}/${FIRMWARE_NAME}_upgrade.bin --reset
 
 # wait for dust to gather
 sleep 5
 
 # get readback upgrade image
-dfu-util -e -d 0020 -a 1 -U ${OUTPUT_DIR}/readback_upgrade.bin
+dfu-util -e -d 4001 -a 1 -U ${OUTPUT_DIR}/readback_upgrade.bin
