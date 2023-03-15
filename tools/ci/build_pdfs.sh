619e71d24010535f3ade4aaa13f6a08f36671f3c
keithm-xmos <35048036+keithm-xmos@users.noreply.github.com>
2023-03-15 15:01:02 -0400

    added support for setting TITLE

--- tools/ci/build_pdfs.sh
+++ tools/ci/build_pdfs.sh
@@ -22,6 +22,7 @@ do
 done
 
 XCORE_VOICE_ROOT=`git rev-parse --show-toplevel`
+DOC_BUILDER_IMAGE=ghcr.io/xmos/doc_builder:v2.0.0
 
 source ${XCORE_VOICE_ROOT}/tools/ci/helper_functions.sh
 
@@ -30,13 +31,13 @@ DIST_DIR=${XCORE_VOICE_ROOT}/dist_pdfs
 mkdir -p ${DIST_DIR}
 
 # setup configurations
-# row format is: "module_path  generate exclude_patterns generated_filename   final_filename"
+# row format is: "module_path  generate exclude_patterns generated_filename   final_filename  title"
 standard_modules=(
-    "modules/io/modules/mic_array   yes programming_guide.pdf   doc_excludes.txt   mic_array_programming_guide.pdf"
-    "modules/io   yes programming_guide.pdf   exclude_patterns.inc   peripheral_io_programming_guide.pdf"
-    "modules/voice  yes user_guide.pdf   exclude_patterns.inc   voice_framework_user_guide.pdf"
-    "modules/rtos   yes programming_guide.pdf   exclude_patterns.inc   rtos_programming_guide.pdf"
-    "modules/rtos   no build_system_guide.pdf   exclude_patterns.inc   build_system_guide.pdf"
+    "modules/io/modules/mic_array   yes programming_guide.pdf   doc_excludes.txt   lib_mic_array_programming_guide.pdf lib\_mic\_array" 
+    "modules/io   yes programming_guide.pdf   exclude_patterns.inc   fwk_io_programming_guide.pdf fwk\_io"
+    "modules/voice  yes user_guide.pdf   exclude_patterns.inc   fwk_voice_programming_guide.pdf fwk\_voice"
+    "modules/rtos   yes programming_guide.pdf   exclude_patterns.inc   rtos_programming_guide.pdf RTOS"
+    "modules/rtos   yes build_system_guide.pdf   exclude_patterns.inc   build_system_user_guide.pdf"
 )
 
 # *****************************************************************
@@ -51,6 +52,7 @@ for ((i = 0; i < ${#standard_modules[@]}; i += 1)); do
     gen_name="${FIELDS[2]}"
     expat_file="${FIELDS[3]}"
     fin_name="${FIELDS[4]}"
+    title="${FIELDS[5]}"
     full_path="${XCORE_VOICE_ROOT}/${rel_path}"
 
     if [[ ${gen_flag} = "yes" ]] ; then
@@ -59,7 +61,7 @@ for ((i = 0; i < ${#standard_modules[@]}; i += 1)); do
         echo '******************************************************'
 
         # build docs
-        (cd ${full_path}; docker run --rm -t -u "$(id -u):$(id -g)" -v $(pwd):/build -e PDF=1 -e REPO:/build -e DOXYGEN_INCLUDE=/build/doc/Doxyfile.inc -e EXCLUDE_PATTERNS=/build/doc/${expat_file} -e DOXYGEN_INPUT=ignore ghcr.io/xmos/doc_builder:v2.0.0)
+        (cd ${full_path}; docker run --rm -t -u "$(id -u):$(id -g)" -v $(pwd):/build -e PDF=1 -e TITLE=${title} -e REPO:/build -e DOXYGEN_INCLUDE=/build/doc/Doxyfile.inc -e EXCLUDE_PATTERNS=/build/doc/${expat_file} -e DOXYGEN_INPUT=ignore ${DOC_BUILDER_IMAGE})
     fi
 
     # copy to dist folder
@@ -76,8 +78,7 @@ echo '******************************************************'
 full_path="${XCORE_VOICE_ROOT}/modules/core/modules/xcore_math/lib_xcore_math"
 
 # build docs
-
-(cd ${full_path}; docker run --rm -t -u "$(id -u):$(id -g)" -v $(pwd):/build -e PDF=1 -e REPO:/build -e DOXYGEN_INCLUDE=/build/doc/Doxyfile.inc -e EXCLUDE_PATTERNS=/build/doc/doc_excludes.txt -e DOXYGEN_INPUT=ignore ghcr.io/xmos/doc_builder:v2.0.0 || echo "Container always falsely reports an error. Ignoring error.")
+(cd ${full_path}; docker run --rm -t -u "$(id -u):$(id -g)" -v $(pwd):/build -e PDF=1 -e TITLE="lib\_xcore\_math" -e REPO:/build -e DOXYGEN_INCLUDE=/build/doc/Doxyfile.inc -e EXCLUDE_PATTERNS=/build/doc/doc_excludes.txt -e DOXYGEN_INPUT=ignore ${DOC_BUILDER_IMAGE} || echo "Container always falsely reports an error. Ignoring error.")
 
 # copy to dist folder
-(cd ${full_path}/doc/_build/pdf; cp programming_guide.pdf ${DIST_DIR}/xcore_math_programming_guide.pdf)
+(cd ${full_path}/doc/_build/pdf; cp programming_guide.pdf ${DIST_DIR}/lib_xcore_math_programming_guide.pdf)
