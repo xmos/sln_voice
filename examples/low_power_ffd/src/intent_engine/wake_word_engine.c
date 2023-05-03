ee952560fb456ab12d04d0388842f06ff27205c6
Jon Carrier <jonathancarrier@xmos.com>
2023-05-03 14:34:53 -0400

    Added wake word engine

---
+++ examples/low_power_ffd/src/intent_engine/wake_word_engine.c
@@ -0,0 +1,91 @@
+// Copyright (c) 2023 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public License: Version 1
+
+/* This unit is responsible for waking up the other tile while in low power
+ * mode, the model used in this unit needs to only detect the wake word.
+ * Requests to enter low power mode are to be made by the other tile. */
+
+/* STD headers */
+#include <platform.h>
+#include <xs1.h>
+
+/* App headers */
+#include "app_conf.h"
+#include "asr.h"
+#include "device_memory_impl.h"
+#include "intent_engine/wake_word_engine.h"
+#include "platform/driver_instances.h"
+#include "power/power_state.h"
+
+#if ON_TILE(AUDIO_PIPELINE_TILE_NO)
+
+// This define is referenced by the model source/header files.
+#ifndef ALIGNED
+#define ALIGNED(x) __attribute__ ((aligned((x))))
+#endif
+
+#define WAKE_WORD_NET_VAR       dnn_wakeword_netLabel
+#define WAKE_WORD_SEARCH_VAR    gs_wakeword_grammarLabel
+
+#ifdef WAKE_WORD_NET_SOURCE_FILE
+#include WAKE_WORD_NET_SOURCE_FILE
+#else
+extern const unsigned short WAKE_WORD_NET_VAR[];
+#endif
+
+#ifdef WAKE_WORD_SEARCH_HEADER_FILE
+#include WAKE_WORD_SEARCH_HEADER_FILE
+#endif
+
+#ifdef WAKE_WORD_SEARCH_SOURCE_FILE
+#include WAKE_WORD_SEARCH_SOURCE_FILE
+#else
+extern const unsigned short WAKE_WORD_SEARCH_VAR[];
+#endif
+
+#define WAKE_WORD_PHRASE    "Hello XMOS"
+#define WAKE_WORD_ID        0x1
+#define IS_WAKE_WORD(id)    ((id) == (WAKE_WORD_ID))
+
+#define SAMPLES_PER_ASR     (appconfINTENT_SAMPLE_BLOCK_LENGTH)
+
+static asr_port_t asr_ctx;
+static devmem_manager_t devmem_ctx;
+
+#pragma stackfunction 50
+void wake_word_engine_init(void)
+{
+    devmem_init(&devmem_ctx);
+    asr_ctx = asr_init((int32_t *)WAKE_WORD_NET_VAR, (int32_t *)WAKE_WORD_SEARCH_VAR, &devmem_ctx);
+    asr_reset(asr_ctx);
+}
+
+#pragma stackfunction 1500
+void wake_word_engine_handler(int32_t *buf, size_t num_frames)
+{
+    asr_result_t asr_result;
+    asr_error_t asr_error;
+    int16_t buf_short[SAMPLES_PER_ASR] = {0};
+
+    for (int i = 0; i < num_frames; i++) {
+        buf_short[i] = buf[i] >> 16;
+    }
+
+    asr_error = asr_process(asr_ctx, buf_short, num_frames);
+
+    if (asr_error == ASR_OK) {
+        asr_error = asr_get_result(asr_ctx, &asr_result);
+    }
+
+    if (asr_error != ASR_OK) {
+        debug_printf("ASR error on tile %d: %d\n", THIS_XCORE_TILE, asr_error);
+        return;
+    }
+
+    if (IS_WAKE_WORD(asr_result.id)) {
+        debug_printf("KEYWORD: " RTOS_STRINGIFY(WAKE_WORD_ID) ", " WAKE_WORD_PHRASE "\n");
+        power_state_set(POWER_STATE_FULL);
+    }
+}
+
+#endif /* ON_TILE(AUDIO_PIPELINE_TILE_NO) */
---
+++ examples/low_power_ffd/src/intent_engine/wake_word_engine.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public License: Version 1
+
+#ifndef WAKE_WORD_ENGINE_H_
+#define WAKE_WORD_ENGINE_H_
+
+#include <stdint.h>
+#include <stddef.h>
+#include "app_conf.h"
+
+#if ON_TILE(AUDIO_PIPELINE_TILE_NO)
+void wake_word_engine_init(void);
+void wake_word_engine_handler(int32_t *buf, size_t num_frames);
+#endif /* ON_TILE(AUDIO_PIPELINE_TILE_NO) */
+
+#endif /* WAKE_WORD_ENGINE_H_ */
