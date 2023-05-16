95cae9beb46dc83b298913210c76032ddeb3ddc4
Jon Carrier <jonathancarrier@xmos.com>
2023-05-16 11:59:14 -0400

    Cleanup of low_power_ffd app_conf.h
    Minor whitespace changes and commenting

--- examples/low_power_ffd/src/app_conf.h
+++ examples/low_power_ffd/src/app_conf.h
@@ -5,22 +5,22 @@
 #define APP_CONF_H_
 
 /* Intertile port settings */
-#define appconfGPIO_T0_RPC_PORT                   1
-#define appconfGPIO_T1_RPC_PORT                   2
-#define appconfINTENT_MODEL_RUNNER_SAMPLES_PORT   3
-#define appconfI2C_MASTER_RPC_PORT                4
-#define appconfI2S_RPC_PORT                       5
-#define appconfCLOCK_CONTROL_PORT                 14
-#define appconfPOWER_CONTROL_PORT                 15
+#define appconfGPIO_T0_RPC_PORT                 1
+#define appconfGPIO_T1_RPC_PORT                 2
+#define appconfINTENT_MODEL_RUNNER_SAMPLES_PORT 3
+#define appconfI2C_MASTER_RPC_PORT              4
+#define appconfI2S_RPC_PORT                     5
+#define appconfCLOCK_CONTROL_PORT               14
+#define appconfPOWER_CONTROL_PORT               15
 
-#define appconfPOWER_STATE_PORT                   8
-#define appconfINTENT_ENGINE_READY_SYNC_PORT      16
+#define appconfPOWER_STATE_PORT                 8
+#define appconfINTENT_ENGINE_READY_SYNC_PORT    16
 
 /* Application tile specifiers */
 #include "platform/driver_instances.h"
-#define AUDIO_PIPELINE_TILE_NO  MICARRAY_TILE_NO
-#define ASR_TILE_NO             FLASH_TILE_NO
-#define FS_TILE_NO              FLASH_TILE_NO
+#define AUDIO_PIPELINE_TILE_NO                  MICARRAY_TILE_NO
+#define ASR_TILE_NO                             FLASH_TILE_NO
+#define FS_TILE_NO                              FLASH_TILE_NO
 
 /* Sensory specific settings */
 #if ON_TILE(ASR_TILE_NO)
@@ -40,8 +40,8 @@
 #define appconfAUDIO_PIPELINE_FRAME_ADVANCE     MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME
 
 /* Intent Engine Configuration */
-#define appconfINTENT_FRAME_BUFFER_MULT      (8*2)       /* total buffer size is this value * MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME */
-#define appconfINTENT_SAMPLE_BLOCK_LENGTH    240
+#define appconfINTENT_FRAME_BUFFER_MULT         (8*2)       /* total buffer size is this value * MIC_ARRAY_CONFIG_SAMPLES_PER_FRAME */
+#define appconfINTENT_SAMPLE_BLOCK_LENGTH       240
 
 /* Enable inference engine */
 #ifndef appconfINTENT_ENABLED
@@ -50,42 +50,42 @@
 
 /* Maximum delay between a wake up phrase and command phrase */
 #ifndef appconfINTENT_RESET_DELAY_MS
-#define appconfINTENT_RESET_DELAY_MS         4000
+#define appconfINTENT_RESET_DELAY_MS            4000
 #endif
 
 /* Maximum number of detected intents to hold */
 #ifndef appconfINTENT_QUEUE_LEN
-#define appconfINTENT_QUEUE_LEN     10
+#define appconfINTENT_QUEUE_LEN                 10
 #endif
 
 /* External wakeup pin edge on intent found.  0 for rising edge, 1 for falling edge */
 #ifndef appconfINTENT_WAKEUP_EDGE_TYPE
-#define appconfINTENT_WAKEUP_EDGE_TYPE     0
+#define appconfINTENT_WAKEUP_EDGE_TYPE          0
 #endif
 
 /* Delay between external wakeup pin edge and intent output */
 #ifndef appconfINTENT_TRANSPORT_DELAY_MS
-#define appconfINTENT_TRANSPORT_DELAY_MS     50
+#define appconfINTENT_TRANSPORT_DELAY_MS        50
 #endif
 
 #ifndef appconfINTENT_I2C_OUTPUT_ENABLED
-#define appconfINTENT_I2C_OUTPUT_ENABLED   1
+#define appconfINTENT_I2C_OUTPUT_ENABLED        1
 #endif
 
 #ifndef appconfINTENT_I2C_OUTPUT_DEVICE_ADDR
-#define appconfINTENT_I2C_OUTPUT_DEVICE_ADDR 0x01
+#define appconfINTENT_I2C_OUTPUT_DEVICE_ADDR    0x01
 #endif
 
 #ifndef appconfINTENT_UART_OUTPUT_ENABLED
-#define appconfINTENT_UART_OUTPUT_ENABLED   1
+#define appconfINTENT_UART_OUTPUT_ENABLED       1
 #endif
 
 #ifndef appconfUART_BAUD_RATE
-#define appconfUART_BAUD_RATE       9600
+#define appconfUART_BAUD_RATE                   9600
 #endif
 
 #ifndef appconfI2S_ENABLED
-#define appconfI2S_ENABLED   1
+#define appconfI2S_ENABLED                      1
 #endif
 
 /* Enable/disable the use of a ring buffer between the audio pipeline and the
@@ -98,29 +98,37 @@
 /* The number of frames to store in the ring buffer, where each frame contains
  * appconfAUDIO_PIPELINE_FRAME_ADVANCE samples. */
 #ifndef appconfAUDIO_PIPELINE_BUFFER_NUM_FRAMES
-#define appconfAUDIO_PIPELINE_BUFFER_NUM_FRAMES 16
+#define appconfAUDIO_PIPELINE_BUFFER_NUM_FRAMES 20
 #endif
 
 #ifndef appconfLOW_POWER_SWITCH_CLK_DIV_ENABLE
 #define appconfLOW_POWER_SWITCH_CLK_DIV_ENABLE  1
 #endif
 
+/* Clock divider used for the switch when in low power. */
 #ifndef appconfLOW_POWER_SWITCH_CLK_DIV
 /* Resulting clock freq: 20MHz */
 #define appconfLOW_POWER_SWITCH_CLK_DIV         30
 #endif
 
+/* Clock divider used to set the low power frequency of the tile being
+ * controlled. During full power, the tile's divider is set to 1. */
 #ifndef appconfLOW_POWER_OTHER_TILE_CLK_DIV
+/* Resulting clock freq: 1MHz */
 #define appconfLOW_POWER_OTHER_TILE_CLK_DIV     600
 #endif
 
+/* Clock divider used to set the nominal frequency of the tile that manages
+ * the "other tile's" frequency. */
 #ifndef appconfLOW_POWER_CONTROL_TILE_CLK_DIV
 /* Resulting clock freq: 300MHz */
 #define appconfLOW_POWER_CONTROL_TILE_CLK_DIV   2
 #endif
 
+/* The time to wait before making a new low power request after the current
+ * request was rejected. */
 #ifndef appconfLOW_POWER_INHIBIT_MS
-#define appconfLOW_POWER_INHIBIT_MS         1000
+#define appconfLOW_POWER_INHIBIT_MS             1000
 #endif
 
 #ifndef appconfAUDIO_PIPELINE_SKIP_IC_AND_VNR
@@ -128,15 +136,15 @@
 #endif
 
 #ifndef appconfAUDIO_PIPELINE_SKIP_NS
-#define appconfAUDIO_PIPELINE_SKIP_NS   0
+#define appconfAUDIO_PIPELINE_SKIP_NS           0
 #endif
 
 #ifndef appconfAUDIO_PIPELINE_SKIP_AGC
-#define appconfAUDIO_PIPELINE_SKIP_AGC   0
+#define appconfAUDIO_PIPELINE_SKIP_AGC          0
 #endif
 
 #ifndef appconfI2S_AUDIO_SAMPLE_RATE
-#define appconfI2S_AUDIO_SAMPLE_RATE appconfAUDIO_PIPELINE_SAMPLE_RATE
+#define appconfI2S_AUDIO_SAMPLE_RATE            appconfAUDIO_PIPELINE_SAMPLE_RATE
 #endif
 
 /* I/O and interrupt cores for Tile 0 */
