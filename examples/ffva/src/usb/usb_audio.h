40118a98db1e79018d8a4cf092aed6c4fdd3f757
lucianomartin <lucianom@xmos.com>
2023-08-22 08:49:06 +0100

    Update copyright notices

--- examples/ffva/src/usb/usb_audio.h
+++ examples/ffva/src/usb/usb_audio.h
@@ -1,4 +1,5 @@
-// Copyright (c) 2021-2022 XMOS LIMITED. This Software is subject to the terms of the
+// Copyright 2021-2023 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
 // XMOS Public License: Version 1
 
 #ifndef USB_AUDIO_H_
