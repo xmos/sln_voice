fb8efa96a6b61eefb12b8ea1892359edae0b784d
Shuchita Khare <shuchitak@xmos.com>
2023-09-29 11:30:02 +0100

    copyright check

--- examples/asrc_demo/src/avg_buffer_level.c
+++ examples/asrc_demo/src/avg_buffer_level.c
@@ -1,3 +1,6 @@
+// Copyright 2023 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
 #define DEBUG_UNIT RATE_SERVER
 #define DEBUG_PRINT_ENABLE_RATE_SERVER 0
 
--- examples/asrc_demo/src/avg_buffer_level.h
+++ examples/asrc_demo/src/avg_buffer_level.h
@@ -1,3 +1,6 @@
+// Copyright 2023 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
 #ifndef AVG_BUFFER_LEVEL_H
 #define AVG_BUFFER_LEVEL_H
 
