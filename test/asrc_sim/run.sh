848e1755cb82f8b7b12e3952cfefc7526aaaac41
Shuchita Khare <shuchitak@xmos.com>
2023-10-04 16:20:33 +0100

    Modified run.sh to run a few cases for testing

--- test/asrc_sim/run.sh
+++ test/asrc_sim/run.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-dir_name=_plots_usb_in_i2s_out_with_correction
+cmake -S . -B ./build
+cmake --build build --target usb_in_i2s_out
+cmake --build build --target i2s_in_usb_out
+
+
+dir_name=_plots
 mkdir -p $dir_name
 
-for i in 192000 176400 96000 88200 48000 44100
-    do
-    build/usb_in_i2s_out $i log_sofs_1hr 2>&1 > log
-    python plot_csv.py log $dir_name/test_correct_$i.png 2
-    done
+# Without SOF timestamps file
+build/usb_in_i2s_out 88200 2>&1 > log
+python calc_snr.py asrc_output.bin 88200
+build/i2s_in_usb_out 192000 2>&1 > log
+python calc_snr.py asrc_output.bin 48000
+
+# With SOF timestamps file
+build/usb_in_i2s_out 176400 log_sofs_1hr 2>&1 > log
+python calc_snr.py asrc_output.bin 176400
+build/i2s_in_usb_out 96000 log_sofs_1hr 2>&1 >  log
+python calc_snr.py asrc_output.bin 48000
+
+#python plot_csv.py log $dir_name/test_correct_$i.png 2
