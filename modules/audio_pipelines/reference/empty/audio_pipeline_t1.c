2943925f82204e04f0851ba8a7c2c8f1068f1192
Luciano Martin <lucianom@xmos.com>
2024-07-08 15:54:44 +0100

    Update empty pipeline in reference group

--- modules/audio_pipelines/reference/empty/audio_pipeline_t1.c
+++ modules/audio_pipelines/reference/empty/audio_pipeline_t1.c
@@ -1,4 +1,4 @@
-// Copyright 2022-2023 XMOS LIMITED.
+// Copyright 2024 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 /* STD headers */
@@ -19,36 +19,26 @@
 /* App headers */
 #include "app_conf.h"
 #include "audio_pipeline.h"
+#include "audio_pipeline_dsp.h"
 
 #if appconfAUDIO_PIPELINE_FRAME_ADVANCE != 240
 #error This pipeline is only configured for 240 frame advance
 #endif
 
-typedef struct {
-    int32_t samples[appconfAUDIO_PIPELINE_CHANNELS][appconfAUDIO_PIPELINE_FRAME_ADVANCE];
-    int32_t aec_reference_audio_samples[appconfAUDIO_PIPELINE_CHANNELS][appconfAUDIO_PIPELINE_FRAME_ADVANCE];
-    int32_t mic_samples_passthrough[appconfAUDIO_PIPELINE_CHANNELS][appconfAUDIO_PIPELINE_FRAME_ADVANCE];
-} frame_data_t;
+#if ON_TILE(1)
 
 static void *audio_pipeline_input_i(void *input_app_data)
 {
     frame_data_t *frame_data;
-
     frame_data = pvPortMalloc(sizeof(frame_data_t));
     memset(frame_data, 0x00, sizeof(frame_data_t));
 
-    size_t bytes_received = 0;
-    bytes_received = rtos_intertile_rx_len(
-            intertile_ctx,
-            appconfAUDIOPIPELINE_PORT,
-            portMAX_DELAY);
+    audio_pipeline_input(input_app_data,
+                       (int32_t **)frame_data->aec_reference_audio_samples,
+                       4,
+                       appconfAUDIO_PIPELINE_FRAME_ADVANCE);
 
-    xassert(bytes_received == sizeof(frame_data_t));
-
-    rtos_intertile_rx_data(
-            intertile_ctx,
-            frame_data,
-            bytes_received);
+    memcpy(frame_data->samples, frame_data->mic_samples_passthrough, sizeof(frame_data->samples));
 
     return frame_data;
 }
@@ -56,10 +46,13 @@ static void *audio_pipeline_input_i(void *input_app_data)
 static int audio_pipeline_output_i(frame_data_t *frame_data,
                                    void *output_app_data)
 {
-    return audio_pipeline_output(output_app_data,
-                               (int32_t **)frame_data->samples,
-                               6,
-                               appconfAUDIO_PIPELINE_FRAME_ADVANCE);
+
+    rtos_intertile_tx(intertile_ctx,
+                      appconfAUDIOPIPELINE_PORT,
+                      frame_data,
+                      sizeof(frame_data_t));
+
+    return AUDIO_PIPELINE_FREE_FRAME;
 }
 
 void empty_stage(void)
@@ -77,7 +70,6 @@ void audio_pipeline_init(
     void *output_app_data)
 {
     const int stage_count = 2;
-
     const pipeline_stage_t stages[] = {
         (pipeline_stage_t)empty_stage,
         (pipeline_stage_t)empty_stage,
@@ -98,4 +90,6 @@ void audio_pipeline_init(
                         (const size_t*) stage_stack_sizes,
                         appconfAUDIO_PIPELINE_TASK_PRIORITY,
                         stage_count);
+
 }
+#endif /* ON_TILE(1)*/
